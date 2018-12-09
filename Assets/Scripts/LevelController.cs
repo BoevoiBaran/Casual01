a1d40b1a971f6d535141d0b6cafb90ff2e1e31b7
BoevoiBaran <alexandr.kudrin.a@gmail.com>
2018-12-09 15:19:36 +0300

    First commit

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField]private GameObject tile;
+
+    [SerializeField] private int levelWidth = 3;
+    [SerializeField] private int levelHeight = 5;
+
+    private void Awake()
+    {
+        //ground = Resources.Load<GroundGreen>("GreenGround");
+    }
+
+    private void Start()
+    {
+        for (int i = -levelWidth; i < levelWidth; i++)
+        {
+            for (int j = -levelHeight; j < levelHeight; j++)
+            {
+                GameObject go = Instantiate(tile, new Vector2(i + 0.5f, j + 0.5f), Quaternion.identity) as GameObject;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private GameObject crystalBlue01;
+
+    private GameObject currentGameObject;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", Random.Range(1, 40), Random.Range(1, 10));
+    }
+
+    private void Spawn()
+    {
+        if(currentGameObject == null)
+        {
+            currentGameObject = Instantiate(crystalBlue01, transform.position, Quaternion.identity);
+        }
+    }
+}
